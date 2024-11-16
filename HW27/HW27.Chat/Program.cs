251fbff8b319ab6ff4bc390d2ce812357f6a43f3
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-16 16:37:13 +0200

    test-version

--- HW27/HW27.Chat/Program.cs
+++ HW27/HW27.Chat/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnectionAsync())
+		using (var channel = connection.Result.CreateChannelAsync())
+		{
+			channel.Result.QueueDeclareAsync(
+				queue: "userQueue",
+				durable: false,
+				exclusive: false,
+				autoDelete: false,
+				arguments: null);
+
+			string message = "Hello RabbitMQ";
+			var body = Encoding.UTF8.GetBytes(message);
+
+			channel.Result.BasicPublishAsync(
+				exchange: "",
+				routingKey: "userQueue",
+				body: body);
+
+			Console.WriteLine($" [x] Sent {message}");
+		}
+
+		Console.ReadLine();
+	}
+}
---
+++ HW27/HW27.Receiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnectionAsync())
+		using (var channel = connection.Result.CreateChannelAsync())
+		{
+			var queueName = "userQueue";
+			channel.Result.QueueDeclareAsync(
+				queue: queueName,
+				durable: false,
+				exclusive: false,
+				autoDelete: false,
+				arguments: null);
+
+            Console.WriteLine("Waiting for messages...");
+
+			var consumer = new AsyncEventingBasicConsumer(channel.Result);
+
+			consumer.ReceivedAsync += async (model, ea) =>
+			{
+				var body = ea.Body.ToArray();
+				var message = Encoding.UTF8.GetString(body);
+				Console.WriteLine($"Received: {message}");
+
+				await Task.CompletedTask;
+			};
+
+			channel.Result.BasicConsumeAsync(
+				queue: queueName,
+				autoAck: true,
+				consumer: consumer);
+
+			Console.ReadLine();
+        }
+	}
+}
